5f019477f796f90bd650485c385307898b715102
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2025-01-08 15:24:10 -0300

    Criação de componentes cards com o uso de @Input e @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']  // Corrigido de styleUrl para styleUrls
 })
 export class AppComponent {
   title = 'meuBinding';
+
+  buttonClicked(){
+    console.log('Clicou no botão!');
+  }
+
+  onCardButtonClicked(){
+    console.log('Você clicou no botão!')
+  }
 }
