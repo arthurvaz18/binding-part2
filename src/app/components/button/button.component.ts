17813c3d6ee98aec9b5d886887a85ff7ae0ae179
Arthur Fernandes Vaz <155276471+arthurvaz18@users.noreply.github.com>
2025-01-12 02:02:45 -0300

    conectando o git

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+
+    @Input({required: true }) 
+    buttonText: string = '';
+
+    @Input({required: true })
+     buttonStyle: 'white' | 'purple' = 'white';
+
+    @Input({alias: 'disabled' })
+     isDisabled: boolean = false;
+
+     @Output('clicked') buttonClickedEmitt = new EventEmitter<void>();
+
+     onButtonClicked(){
+      this.buttonClickedEmitt.emit();
+     }
+}
